6b6eb08dfbc95fe94494cebc600aadb742b2dc9f
Nafitafi <nafibriones@gmail.com>
2026-02-14 21:49:19 -0700

    Creación del archivo rpincipal del proyecto

---
+++ proyecto1_BDA/src/main/java/itson/proyecto1_bda/Proyecto1_BDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.proyecto1_bda;
+
+/**
+ *
+ * @author nafbr
+ */
+public class Proyecto1_BDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
