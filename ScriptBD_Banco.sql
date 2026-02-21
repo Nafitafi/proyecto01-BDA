e317eff5ccdf553979d76eaa995cf98b344c7d1f
Oliver Robles <oli.rob2948@gmail.com>
2026-02-20 20:29:08 -0700

    Update DB

---
+++ ScriptBD_Banco.sql
@@ -0,0 +1,134 @@
+DROP DATABASE IF EXISTS banco;
+CREATE DATABASE banco;
+USE banco;
+
+-- Tablas
+CREATE TABLE direcciones_clientes (
+    id_direccion INT AUTO_INCREMENT PRIMARY KEY,
+    calle VARCHAR(50) NOT NULL,
+    numero VARCHAR(10) NOT NULL,
+    colonia VARCHAR(50) NOT NULL,
+    codigo_postal VARCHAR(10) NOT NULL,
+    ciudad VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE clientes (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nombres VARCHAR(50) NOT NULL,
+    apellido_paterno VARCHAR(50) NOT NULL,
+    apellido_materno VARCHAR(50) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    edad INT, -- Se calcula con Trigger
+    correo VARCHAR(50) NOT NULL UNIQUE,
+    contrasena VARCHAR(255) NOT NULL,
+    id_direccion INT NOT NULL,
+    FOREIGN KEY (id_direccion) REFERENCES direcciones_clientes(id_direccion)
+);
+
+CREATE TABLE cuentas (
+    numero_cuenta VARCHAR(12) PRIMARY KEY,
+    fecha_apertura DATE NOT NULL,
+    saldo DECIMAL(10,2) NOT NULL CHECK (saldo >= 0),
+    estado ENUM('activa', 'bloqueada', 'cerrada') NOT NULL,
+    id_cliente INT NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
+);
+
+CREATE TABLE operaciones (
+    folio_operacion INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_operacion ENUM('transferencia', 'retiro sin cuenta') NOT NULL,
+    fecha_operacion DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    monto DECIMAL(10,2) NOT NULL CHECK (monto > 0),
+    descripcion VARCHAR(100),
+    numero_cuenta VARCHAR(12) NOT NULL,
+    FOREIGN KEY (numero_cuenta) REFERENCES cuentas(numero_cuenta)
+) AUTO_INCREMENT = 10000000;
+
+CREATE TABLE transferencias (
+	folio_operacion INT PRIMARY KEY,
+    cuenta_destino VARCHAR(12) NOT NULL,
+    fecha_transferencia DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (folio_operacion) REFERENCES operaciones(folio_operacion),
+    FOREIGN KEY (cuenta_destino) REFERENCES cuentas(numero_cuenta)
+);
+
+CREATE TABLE retiros_sin_cuenta (
+    folio_operacion INT PRIMARY KEY,
+    contrasena_retiro VARCHAR(8) NOT NULL,
+    fecha_retiro DATETIME NOT NULL,
+    estado_retiro ENUM('pendiente', 'cobrado', 'cancelado') NOT NULL,
+    FOREIGN KEY (folio_operacion) REFERENCES operaciones(folio_operacion)
+);
+
+-- Triggers
+-- Calcular Edad automáticamente al insertar cliente
+DELIMITER $$
+CREATE TRIGGER calcular_edad_insert
+BEFORE INSERT ON clientes
+FOR EACH ROW
+BEGIN
+    SET NEW.edad = TIMESTAMPDIFF(YEAR, NEW.fecha_nacimiento, CURDATE());
+END$$
+DELIMITER ;
+
+-- Stored procedure realizar transferencia
+DELIMITER $$
+CREATE PROCEDURE realizar_transferencia (
+	IN p_cuenta_origen VARCHAR(12),
+    IN p_cuenta_destino VARCHAR(12),
+    IN p_monto DECIMAL(10,2),
+    OUT p_folio_generado INT
+)
+BEGIN
+	
+    -- Declaramos las variables para uso local
+    DECLARE v_id_op INT;
+    DECLARE v_saldo_actual DECIMAL(10,2);
+    
+    -- En caso de fallo hacemos un rollback y seteamos el mensage de error
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+	 	ROLLBACK;
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "ERROR: No fue posible completar la transferencia.";
+	END;
+    
+    -- Iniciamos la transaccion
+    START TRANSACTION;
+    
+    -- Guardamos el saldo actual de la cuenta origen en la variable creada
+    -- Ademas secuentramos la fila de la cuenta origen para que ningun otro proceso pueda modificarla hasta que esta transacción termine.
+    SELECT saldo INTO v_saldo_actual FROM cuentas WHERE numero_cuenta = p_cuenta_origen FOR UPDATE;
+    
+    -- Procedemos solo si la cuenta origen tiene el saldo suficiente para transferir.
+    IF v_saldo_actual >= p_monto THEN
+		
+        -- Restamos el monto a la cuenta origen. 
+		UPDATE cuentas SET saldo = saldo - p_monto WHERE numero_cuenta = p_cuenta_origen;
+        
+        -- Incrementamos el monto a la cuenta destino.
+        UPDATE cuentas SET saldo = saldo + p_monto WHERE numero_cuenta = p_cuenta_destino;
+        
+        -- Creamos el registro de la operacion 
+        -- NOW proporciona fecha y la hora
+        INSERT INTO operaciones (tipo_operacion, fecha_operacion, monto, descripcion, numero_cuenta)
+        VALUES ('transferencia', NOW(), p_monto, CONCAT("Transferencia enviada a la cuenta: ", p_cuenta_destino), p_cuenta_origen);
+        
+        SET v_id_op = LAST_INSERT_ID();
+        
+        -- Creamos el registro de la transferencia
+        INSERT INTO transferencias (folio_operacion, cuenta_destino, fecha_transferencia)
+        VALUES (v_id_op, p_cuenta_destino, NOW());
+        
+	    -- Ejecutamos la sentencia completa
+        COMMIT;
+        
+        -- Asignamos el folio al parametro de salida
+        SET p_folio_generado = v_id_op;
+        
+	ELSE
+		SIGNAL SQLSTATE '45000';
+		ROLLBACK;
+    END IF;
+END$$
+DELIMITER ;
+
---
+++ ScriptPruebas_Banco.sql
@@ -0,0 +1,50 @@
+-- Datitos prueba
+-- Direcciones
+INSERT INTO direcciones_clientes (calle, numero, colonia, codigo_postal, ciudad) VALUES
+('Aguila', '502', 'Nueva Palmira', '85190', 'Ciudad Obregón'),
+('Calle Guerrero', '456', 'Torres De Paris', '85160', 'Ciudad Obregón'),
+('Av. Miguel Alemán', '789', 'Villa Bonita', '85090', 'Ciudad Obregón');
+
+-- Clientes (Contraseñas en texto plano POR AHORA)
+INSERT INTO clientes (nombres, apellido_paterno, apellido_materno, fecha_nacimiento, correo, contrasena, id_direccion) VALUES
+('Emily', 'Lara', 'García', '2006-04-15', 'emily@gmail.com', '123456', 1),
+('Oliver', 'Robles', 'Cota', '2006-05-12', 'oliver@gmail.com', '123456', 2),
+('Nahomi', 'Figueroa', 'Briones', '2006-07-14', 'nahomi@gmail.com', '123456', 3);
+
+INSERT INTO cuentas (numero_cuenta, fecha_apertura, saldo, estado, id_cliente) VALUES
+('123-456-7890', '2023-01-10', 5000.00, 'activa', 1),
+('234-567-8901', '2023-02-15', 300.50, 'activa', 2),
+('345-678-9012', '2023-03-20', 10000.00, 'activa', 3);
+
+-- Transferenecnias de prueba
+UPDATE cuentas
+SET saldo = saldo - 10
+WHERE numero_cuenta = '123-456-7890';
+
+UPDATE cuentas
+SET saldo = saldo + 10
+WHERE numero_cuenta = '345-678-9012';
+
+INSERT INTO operaciones (tipo_operacion, monto, descripcion, numero_cuenta)
+VALUES ("transferencia", 10, "Operacion exitosa",'234-567-8901');
+
+-- Busquedas de prueba
+SELECT numero_cuenta, saldo, estado, fecha_apertura, id_cliente
+FROM cuentas WHERE numero_cuenta = '234-567-8901';
+
+SELECT numero_cuenta, saldo, estado, fecha_apertura, id_cliente
+FROM cuentas;
+
+SELECT *
+FROM operaciones;
+
+-- Prueba del stored procedured
+CALL realizar_transferencia("123-456-7890", "234-567-8901", 300);
+SELECT * FROM cuentas;
+SELECT * FROM operaciones;
+SELECT * FROM transferencias;
+
+SELECT saldo FROM cuentas WHERE numero_cuenta = "123-456-7890";
+
+SELECT VERSION();
+
